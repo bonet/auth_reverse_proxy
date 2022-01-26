09656ae89df8d20ff4880afeda884ba6bbf14d03
Bonet Sugiarto <bonetsu@gmail.com>
2022-01-26 23:23:04 +0700

    Authentication APIs

--- auth/app.py
+++ auth/app.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, redirect, Response, render_template
+from flask import Flask, request, redirect, Response, render_template, jsonify, make_response
 from flask_login import login_required, login_user, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-import requests, random
+import requests, random, string
+from datetime import datetime as dt
 
 from libs.extensions import db, migrate
 
 from user.models import User
+from partner.models import Partner
 from oauth2_token.models import OAuth2Token
 
 app = Flask(__name__)
@@ -23,47 +25,58 @@ def index():
 
 @app.route('/api/v1/auth',methods=['GET', 'POST'])
 def login():
-    callback_url=request.args.get('callback_url')
-    client_access_token=request.args.get('client_access_token')
 
     if request.method == 'POST':
+        callback_url=request.form.get('callback_url')
+        client_access_token=request.form.get('client_access_token')
+        partner_id=request.form.get('partner_id')
+
         user = User.query.filter(
-            User.email==request.args.get('email'),
-            User.password==request.args.get('password').encode('ascii')
+            User.email==request.form.get('email'),
+            User.password==request.form.get('password').encode('ascii')
             ).one_or_none()
 
-        auth_code = ''.join(random.SystemRandom().choice(string.hexdigits) for _ in range(32))
-        oauth2_token = OAuth2Token.query.filter(OAuth2Token.user_id == user.id).one_or_none()
-        if oauth2_token:
+        partner = Partner.query.filter(
+            Partner.id==partner_id
+            ).one_or_none()
+
+        if(user and partner):
+            auth_code = ''.join(random.SystemRandom().choice(string.hexdigits) for _ in range(32))
+            access_token = ''.join(random.SystemRandom().choice(string.hexdigits) for _ in range(32))
+            oauth2_token = OAuth2Token.query.filter(OAuth2Token.user_id == user.id).one_or_none()
+
+            oauth2_token = oauth2_token or OAuth2Token()
+            oauth2_token.user_id = user.id
+            oauth2_token.partner_id = partner.id
             oauth2_token.auth_code = auth_code
-        else:
-            oauth2_token = OAuth2Token(
-                    user_id = user.id,
-                    auth_code = auth_code
-                )
+            oauth2_token.access_token = access_token
+            oauth2_token.issued_at = dt.utcnow().timestamp()
+            oauth2_token.expires_in = 3600
 
-        db.session.add(oauth2_token)
+            db.session.add(oauth2_token)
+            db.session.commit()
 
-        register_user = (user.fullname == None ||
-            user.address == None ||
-            user.phonw == None) ? 1 : 0
+            register_user = 1 if (user.fullname == None or
+                user.address == None or
+                user.phone == None) else 0
 
-        if(user):
-            return redirect("/form/v1/allow_form",
-                    auth_code=auth_code,
-                    callback_url=callback_url,
-                    client_access_token=client_access_token
-                )
+            return redirect(f"/form/v1/allow_form?auth_code={auth_code}&callback_url={callback_url}&client_access_token={client_access_token}&register_user={register_user}")
         else:
             return render_template("login_form.html",
                     callback_url=callback_url,
-                    client_access_token=client_access_token
+                    client_access_token=client_access_token,
+                    partner_id=partner_id
                 )
 
     else:
+        callback_url=request.args.get('callback_url')
+        client_access_token=request.args.get('client_access_token')
+        partner_id=request.args.get('partner_id')
+
         return render_template("login_form.html",
                 callback_url=callback_url,
-                client_access_token=client_access_token
+                client_access_token=client_access_token,
+                partner_id=partner_id
             )
 
 
@@ -71,15 +84,36 @@ def login():
 def allow_form():
     callback_url=request.args.get('callback_url')
     client_access_token=request.args.get('client_access_token')
+    auth_code=request.args.get('auth_code')
+    register_user=request.args.get('register_user')
 
     return render_template("allow_form.html",
+            auth_code=auth_code,
             callback_url=callback_url,
             client_access_token=client_access_token,
             register_user=register_user
         )
 
 
+# Ideally this is Partner's callback API, not client-facing API
+# Partner server will be whitelisted on our backend server address
 @app.route('/api/v1/get_token',methods=['POST'])
 def get_token():
-    auth_code = request.args.get('auth_code')
-    oauth2_token = OAuth2Token.query.filter(OAuth2Token.user_id == user.id).one_or_none()
+    auth_code = request.form.get('auth_code')
+    client_id = request.form.get('client_id')
+    partner_id = request.form.get('partner_id')
+    partner_secret_key = request.form.get('partner_secret_key')
+    user_id = request.form.get('user_id')
+    register_user = request.form.get('register_user')
+    full_name = request.form.get('full_name')
+    address = request.form.get('address')
+    phone = request.form.get('phone')
+
+    oauth2_token = OAuth2Token.query.filter(OAuth2Token.user_id == user_id,
+        OAuth2Token.auth_code == auth_code
+        ).one_or_none()
+
+    return make_response(jsonify({
+                'access_token': oauth2_token.access_token,
+                'expire': (oauth2_token.issued_at + 3600)
+            }), 200)
