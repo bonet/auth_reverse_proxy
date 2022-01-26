c6c0bf7b999952908aa1aec4d3bad2012437c52f
Bonet Sugiarto <bonetsu@gmail.com>
2022-01-26 07:41:47 +0700

    Add libs stuff

---
+++ auth/libs/database.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""Database module, including the SQLAlchemy database object and DB-related utilities."""
+from extensions import db
+from sqlalchemy.orm import class_mapper, ColumnProperty
+from sqlalchemy.types import UserDefinedType
+
+# Alias common SQLAlchemy names
+Column = db.Column
+relationship = db.relationship
+
+
+class BaseMixin(object):
+    def as_dict(self, selected_columns=[]):
+        result = {}
+        for prop in class_mapper(self.__class__).iterate_properties:
+            if isinstance(prop, ColumnProperty):
+                if selected_columns == [] or (selected_columns!=[] and prop.key in selected_columns):
+                    result[prop.key] = getattr(self, prop.key)
+        return result
+
+
+class CRUDMixin(object):
+    """Mixin that adds convenience methods for CRUD (create, read, update, delete) operations."""
+
+    @classmethod
+    def create(cls, **kwargs):
+        """Create a new record and save it the database."""
+        instance = cls(**kwargs)
+        return instance.save()
+
+    def update(self, commit=True, **kwargs):
+        """Update specific fields of a record."""
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+        return commit and self.save() or self
+
+    def save(self, commit=True):
+        """Save the record."""
+        db.session.add(self)
+        if commit:
+            db.session.commit()
+        return self
+
+    def delete(self, commit=True):
+        """Remove the record from the database."""
+        db.session.delete(self)
+        return commit and db.session.commit()
+
+
+class Model(CRUDMixin, db.Model, BaseMixin):
+    """Base model class that includes CRUD convenience methods."""
+
+    __abstract__ = True
+
+
+# From Mike Bayer's "Building the app" talk
+# https://speakerdeck.com/zzzeek/building-the-app
+class SurrogatePK(object):
+    """A mixin that adds a surrogate integer 'primary key' column named ``id`` to any declarative-mapped class."""
+
+    __table_args__ = {"extend_existing": True}
+
+    id = Column(db.Integer, primary_key=True)
+
+
+
+def reference_col(
+    tablename, nullable=False, pk_name="id", foreign_key_kwargs=None, column_kwargs=None
+):
+    """Column that adds primary key foreign key reference.
+
+    Usage: ::
+
+        category_id = reference_col('category')
+        category = relationship('Category', backref='categories')
+    """
+    foreign_key_kwargs = foreign_key_kwargs or {}
+    column_kwargs = column_kwargs or {}
+
+    return Column(
+        db.ForeignKey(f"{tablename}.{pk_name}", **foreign_key_kwargs),
+        nullable=nullable,
+        **column_kwargs,
+    )
