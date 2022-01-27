db93f0e975a83e6d8b7b6d56bccabc9f7e8b9150
Bonet Sugiarto <bonetsu@gmail.com>
2022-01-27 09:58:51 +0700

    Add reverse proxy controler in app.py

--- reverse_proxy/app.py
+++ reverse_proxy/app.py
@@ -5,6 +5,8 @@ from datetime import datetime as dt
 
 from libs.extensions import db
 
+from oauth2_token.models import OAuth2Token
+
 app = Flask(__name__)
 app.config.from_object("config.settings")
 db.init_app(app)
@@ -12,6 +14,40 @@ db.init_app(app)
 
 @app.route('/')
 def index():
+    OAuth2Token.find_by_access_token(db.session, '', '')
     return 'Flask is running!'
 
 
+@app.route('/<path:path>',methods=['GET','POST','DELETE'])
+def proxy(path):
+    global SITE_NAME
+    user_id = request.headers.get('UID')
+    access_token = None
+    auth_header = request.headers.get('Authorization')
+
+    if auth_header:
+        auth_header_arr = auth_header.split()
+        access_token = auth_header_arr[-1]
+
+    if user_id and access_token:
+        token = OAuth2Token.find_by_access_token(db.session, access_token, user_id)
+        if request.method=='GET':
+            resp = requests.get(f'{SITE_NAME}{path}')
+            excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+            headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
+            response = Response(resp.content, resp.status_code, headers)
+            return response
+        elif request.method=='POST':
+            resp = requests.post(f'{SITE_NAME}{path}',json=request.get_json())
+            excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+            headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+            response = Response(resp.content, resp.status_code, headers)
+            return response
+        elif request.method=='DELETE':
+            resp = requests.delete(f'{SITE_NAME}{path}').content
+            response = Response(resp.content, resp.status_code, headers)
+            return response
+    else:
+        return make_response(jsonify({
+                'Status': 'Error'
+            }), 401)
